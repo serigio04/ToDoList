c9231cdff7e7f287bba7b28a0bb3880971b2bdb0
serigio04 <serigio.gomar15@gmail.com>
2023-12-07 21:53:25 -0600

    Agregando un módulo de prioridad

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
     const list = document.querySelector('[data-list]');
     const task = document.createElement("li");
     task.classList.add("card");
-    value = " ";
+    input.value = " ";
     //backlist
     const taskContent = document.createElement("div");
     taskContent.appendChild(checkComplete());
@@ -17,13 +17,36 @@
     tittleTask.classList.add("task");
     tittleTask.innerHTML = value;
     taskContent.appendChild(tittleTask)
-    //task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(addPriority())
     task.appendChild(deleteBtn());
     list.appendChild(task);
   }
   
   btn.addEventListener('click', createTask);
+
+  const addPriority = () =>{
+    const Select_priority = document.querySelector('[data-form-priority]');
+    const priority = Number(Select_priority.value)
+    let level
+    switch (priority){
+      case 1:
+        level = "High"
+        break
+      case 2:
+        level = "Middle"
+        break
+      case 3: 
+        level = "Low"
+        break
+      default:
+        level = "un"
+        break
+    }
+    const priText = document.createElement("span")
+    priText.innerText = level
+    return priText 
+  }
 
   const checkComplete = () => {
     const i = document.createElement("i");
